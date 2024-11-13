eab9742289c9021383545abf9595f19fffe87d6c
Antwhale <olollolo339@naver.com>
2024-11-14 01:35:30 +0900

    Initial Commit

---
+++ SampleSwiftUI01/MyVstack.swift
@@ -0,0 +1,8 @@
+//
+//  MyVstack.swift
+//  SampleSwiftUI01
+//
+//  Created by 부재식 on 11/14/24.
+//
+
+import Foundation
---
+++ SampleSwiftUI01/SampleSwiftUI01App.swift
@@ -0,0 +1,17 @@
+//
+//  SampleSwiftUI01App.swift
+//  SampleSwiftUI01
+//
+//  Created by 부재식 on 11/14/24.
+//
+
+import SwiftUI
+
+@main
+struct SampleSwiftUI01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
